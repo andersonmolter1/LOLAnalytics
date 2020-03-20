117ad586a30d895242aedeaf44221d6af934d1d2
Anderson Molter <andersonmolter1@gmail.com>
2020-03-20 19:04:04 -0400

    Active Changes

--- GetMatchData.py
+++ GetMatchData.py
@@ -1,8 +1,7 @@
 import requests
 import json
-dataRequest = requests.get('https://na1.api.riotgames.com/lol/match/v4/timelines/by-match/3008888833?api_key=RGAPI-4c01dd81-4dc2-4ce8-9d0a-716b12124110')
+dataRequest = requests.get('https://na1.api.riotgames.com/lol/match/v4/timelines/by-match/3008888833?api_key=RGAPI-187e7282-6283-4d1b-915f-66479d618862')
 matchData = dataRequest.text
-matchData = json.loads(matchData)
-for i in 
+matchData = json.loads(matchData) 
 with open('sample.json', 'w') as outfile:
         json.dump(matchData, outfile)
